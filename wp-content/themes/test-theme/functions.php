1af80223ad2f66ba60189004809e8aa8d1b7ff19
Dylan Ng <ngxinde@gmail.com>
2016-10-06 17:20:49 +0800

    Post looping and custom body classes

--- wp-content/themes/test-theme/functions.php
+++ wp-content/themes/test-theme/functions.php
@@ -8,7 +8,6 @@
     wp_enqueue_style( 'test-style', get_template_directory_uri() . '/css/test.css', array(), '1.0.0', 'all' );
     wp_enqueue_script( 'test-js', get_template_directory_uri() . '/js/test.js', array('jquery'), '1.0.0', true );
   }
-
   // Add function when wordpress is enquing scripts
   add_action( 'wp_enqueue_scripts', 'test_script_enqueue' );
 
--- wp-content/themes/test-theme/header.php
+++ wp-content/themes/test-theme/header.php
@@ -6,7 +6,14 @@
     <?php wp_head(); ?>
   </head>
 
-  <body>
+  <?php
+    if( is_front_page() ):
+      $tehBody = ['root-body', 'awesome'];
+    else:
+      $tehBody = ['not-root-body'];
+    endif;
+  ?>
+  <body <?php body_class( $tehBody ); ?>>
     <!-- Displays nav menu -->
     <?php wp_nav_menu(array('container'=>'nav', 'theme_location'=>'primary')); ?>
 <!-- end of header -->
